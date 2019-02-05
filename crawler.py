cb2b43aa35e2fbf427229483bd0de2999ce4303b
Marcin Zajac <m.zajac995@gmail.com>
2019-02-05 23:16:00 +0100

    Two loops into one, added some comments

--- crawler.py
+++ crawler.py
@@ -1,52 +1,45 @@
 import mechanize
 import urllib.parse
-from urllib.parse import urlparse
 
 
 def is_not_absolute(url):
+    # Check if url is an absolute or not
     if '//' not in url:
         return True
     else:
         return False
 
 
-def map(url):
+def map_site(url):
     br = mechanize.Browser()
     br.set_handle_robots(False)
     try:
         br.open(url)
     except:
-        return {}
-    # Gives url of hostname
-    parsed_uri = urlparse(url)
+        return "Given url wasn't valid."
+    # Gives url of hostname, so given url can be any subpage of domain
+    parsed_uri = urllib.parse.urlparse(url)
     host_url = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
     list_url = [host_url]
-    for link_item in list_url:
-        br.open(link_item)
-        for link in br.links():
-            link = link.url
-            if is_not_absolute(link):
-                link = urllib.parse.urljoin(host_url, link)
-            if '{uri.netloc}/'.format(uri=parsed_uri) in link and link not in list_url:
-                    list_url.append(link)
-    my_dict = {
-
-    }
-    for page in list_url:
-        br.open(page)
-        title = br.title()
+    my_dict = {}
+    # Loop through pages from our domain
+    for list_item in list_url:
+        br.open(list_item)
         links = set()
+        # Loop through urls from specific page
         for link in br.links():
             link = link.url
             if is_not_absolute(link):
                 link = urllib.parse.urljoin(host_url, link)
+            # Check if link is not link to external site
             if '{uri.netloc}/'.format(uri=parsed_uri) in link:
                 links.add(link)
-
+                if link not in list_url:
+                    list_url.append(link)
         dict_item = {
-            page: {
-                'title': title,
+            list_item: {
+                'title': br.title(),
                 'links': links}
-        }
+            }
         my_dict.update(dict_item)
     return my_dict
