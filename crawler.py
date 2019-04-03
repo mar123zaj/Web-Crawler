b3bae795a101e58428272a615d352288b893c75d
Marcin Zajac <m.zajac995@gmail.com>
2019-04-03 21:42:35 +0200

    Added README file, example site for testing and made some changes to crawler.py and tests.py

--- crawler.py
+++ crawler.py
@@ -1,70 +1,93 @@
 import mechanize
-from collections import namedtuple
+import sys
+import pprint
+import collections
 from urllib.parse import urljoin, urlparse
 
-def validated(url):
-    '''Checks if given url is proper.'''
-    browser = mechanize.Browser()
-    browser.set_handle_robots(False)
-    try:
-        browser.open(url)
+
+def is_validated(url):
+    """Checks if given url is proper."""
+    parsed_url = urlparse(url)
+    if parsed_url.scheme and parsed_url.netloc:
         return True
-    except:
+    else:
         return False
 
-def validated_link(link, host_url):
-    '''Check if link is relative and make it absolute.'''
-    if '//' not in link:
-        return urljoin(host_url, link)
+
+def make_url_absolute(url, domain_full_address):
+    """Check if link is relative and make it absolute."""
+    if "//" not in url:
+        return urljoin(domain_full_address, url)
     else:
-        return link
- 
- 
-def page_properties(page_url, domain, netloc):
-    '''Returns page properties:
-        all links from specific domain and page title.'''
-    
+        return url
+
+
+def domain_tuple(url):
+    """Returns namedtuple with full domain address and domain name of given url."""
+    parsed_url = urlparse(url)
+    Domain = collections.namedtuple("Domain", "full_address, name")
+    return Domain(f"{parsed_url.scheme}://{parsed_url.netloc}", parsed_url.netloc)
+
+
+def page_title(url):
+    """Returns title of given url."""
     browser = mechanize.Browser()
     browser.set_handle_robots(False)
-    browser.open(page_url)
+    browser.open(url)
+    return browser.title()
+
+
+def page_links(url):
+    """Returns all links from given page."""
+    browser = mechanize.Browser()
+    browser.set_handle_robots(False)
+    browser.open(url)
+    domain = domain_tuple(url)
     links = set()
     for link in browser.links():
-        link = validated_link(link.url, domain)
-        # Check if link is not link to external site
-        if netloc in link:
-            links.add(link)
-    return [links, browser.title()]
+        link = make_url_absolute(link.url, domain.full_address)
+        links.add(link)
+    return links
 
 
-def map_page(page_url, domain, netloc):
-    '''Returns dictionary of page properties and page url.'''
-    links, title = page_properties(page_url, domain, netloc)
+def filtered_out_links(links, domain_name):
+    """Returns set of links that are only links from given domain."""
+    filtered_links = set()
+    # Check if link is not link to external site
+    for link in links:
+        if domain_name in link:
+            filtered_links.add(link)
+    return filtered_links
 
-    return {
-            page_url: {
-                'title': title,
-                'links': links}
-            }
+
+def updated_urls_list(urls, links):
+    """Upade urls list with new links."""
+    for link in links:
+        if link not in urls:
+            urls.append(link)
+    return urls
 
 
-def map_site(url):
-    '''Returns dictionary of all pages properties from specific domain.'''
-    if validated(url):
-        parsed_url = urlparse(url)
-        Domain = namedtuple('Domain', 'domain, netloc')
-        url = Domain(parsed_url.scheme + '://' + parsed_url.netloc,
-                         parsed_url.netloc)
-        urls = [url.domain, ]  # comma added for more clarity
+def site_map(url):
+    """Returns dictionary which will return page title and links of page from given domain."""
+    if is_validated(url):
+        domain = domain_tuple(url)
+        urls = [domain.full_address]
         site_mapping = dict()
+
         # Loop through pages from our domain
-        for url_ in urls:
-            page_mapping = map_page(url_, url.domain, url.netloc)
-            site_mapping.update(page_mapping)
-            for link in page_mapping[url_]['links']:
-                if link not in urls: urls.append(link)
+        for page_url in urls:
+            all_links = page_links(page_url)
+            links = filtered_out_links(all_links, domain.name)
+            title = page_title(page_url)
+            site_mapping.update({page_url: {"title": title, "links": links}})
+
+            urls = updated_urls_list(urls, links)
         return site_mapping
     else:
         return "URL wasn't valid!"
 
-#print(map_site('http://mar123zaj.pythonanywhere.com/'))
-print(map_site('http://localhost:8000/'))
+
+if __name__ == "__main__":
+    arg = sys.argv[1]
+    pprint.pprint(site_map(arg))
--- tests.py
+++ tests.py
@@ -1,51 +1,128 @@
-from crawler import validated, validated_link, page_properties, map_page, map_site
-from urllib.parse import urlparse
+from crawler import (
+    is_validated,
+    make_url_absolute,
+    filtered_out_links,
+    updated_urls_list,
+    page_title,
+    page_links,
+    site_map,
+)
 
+
+def test_is_validated0():
+    input_ = "https://mail.google.com/mail/"
+    assert is_validated(input_) == True
+
+
+def test_is_validated1():
+    input_ = "google.com"
+    assert is_validated(input_) == False
+
+
+def test_make_url_absolute0():
+    url = "https://github.com/mar123zaj/Web-Crawler"
+    domain_address = "https://github.com"
+    assert (
+        make_url_absolute(url, domain_address)
+        == "https://github.com/mar123zaj/Web-Crawler"
+    )
+
+
+def test_make_url_absolute1():
+    url = "/mar123zaj/Web-Crawler"
+    domain_address = "https://github.com"
+    assert (
+        make_url_absolute(url, domain_address)
+        == "https://github.com/mar123zaj/Web-Crawler"
+    )
+
+
+def test_filtered_out_links():
+    links_set = {
+        "http://localhost:8000/example.html",
+        "http://localhost:8000/site.html",
+        "https://clearcode.pl/kariera/staze/",
+    }
+    domain_name = "http://localhost:8000/"
+    assert filtered_out_links(links_set, domain_name) == {
+        "http://localhost:8000/example.html",
+        "http://localhost:8000/site.html",
+    }
+
+
+def test_updated_urls_list():
+    urls_list = [
+        "http://localhost:8000/",
+        "http://localhost:8000/example.html",
+        "http://localhost:8000/site.html",
+    ]
+    links_list = [
+        "http://localhost:8000/site/subsite.html",
+        "http://localhost:8000/example.html",
+        "http://localhost:8000/site/other_site.html",
+    ]
+    assert updated_urls_list(urls_list, links_list) == [
+        "http://localhost:8000/",
+        "http://localhost:8000/example.html",
+        "http://localhost:8000/site.html",
+        "http://localhost:8000/site/subsite.html",
+        "http://localhost:8000/site/other_site.html",
+    ]
+
+
+def test_page_title():
+    input_ = "http://localhost:8000/site/other_site.html"
+    assert page_title(input_) == "Looped"
+
+
+def test_page_links():
+    link = "http://localhost:8000/site/subsite.html"
+    assert page_links(link) == {
+        "http://localhost:8000/site/other_site.html",
+        "http://localhost:8000/site/other_site.html",
+        "http://localhost:8000/",
+    }
 
 
 def test_site_map():
-    pass
+    input_ = "google.com"
+    assert site_map(input_) == "URL wasn't valid!"
 
 
-def test_url_validated0():
-    input_ = 'abcd'
-    assert validated(input_) == False
- 
-def test_url_validated1():
-    input_ = 'https://docs.python.org/3/whatsnew/3.7.html'
-    assert validated(input_) == True
-
-def test_validated_link0():
-    link1 = '/CodingDojoSilesia/bank-ocr'
-    domain_url = 'https://github.com'
-    assert validated_link(link1, domain_url) == 'https://github.com/CodingDojoSilesia/bank-ocr'
-
-
-def test_validated_link0():
-    link1 = 'https://www.facebook.com/user123'
-    domain_url = 'https://github.com'
-    assert validated_link(link1, domain_url) == 'https://www.facebook.com/user123'
-
-
-def test_page_properties():
-    page_url = 'http://www.madore.org/~david/.test/uni.html' # small page for testing purposes
-    parsed_url = urlparse(page_url)
-    domain = '{parsed_url.scheme}://{parsed_url.netloc}'
-    netloc = parsed_url.netloc
-    assert page_properties(page_url, domain, netloc) == [set(), 'A small test page']
-
-
-def test_map_page():
-    page_url = 'http://www.madore.org/~david/.test/uni.html' # small page for testing purposes
-    parsed_url = urlparse(page_url)
-    domain = '{parsed_url.scheme}://{parsed_url.netloc}'
-    netloc = parsed_url.netloc
-    assert map_page(page_url, domain, netloc) == {'http://www.madore.org/~david/.test/uni.html': {
-                                                                                        'title': 'A small test page',                                                                                         
-                                                                                        'links': set()}
-                                                                            }
-
-
-def test_map_site():
-    input_ = 'http://localhost:8000/'
-    assert map_site(input_) == {'http://localhost:8000': {'title': 'Index', 'links': {'http://localhost:8000/site.html', 'http://localhost:8000/example.html'}}, 'http://localhost:8000/site.html': {'title': 'The Site', 'links': {'http://localhost:8000/site/subsite.html'}}, 'http://localhost:8000/example.html': {'title': 'No links here', 'links': set()}, 'http://localhost:8000/site/subsite.html': {'title': 'Looping', 'links': {'http://localhost:8000/site/other_site.html', 'http://localhost:8000/'}}, 'http://localhost:8000/site/other_site.html': {'title': 'Looped', 'links': {'http://localhost:8000/site/subsite.html'}}, 'http://localhost:8000/': {'title': 'Index', 'links': {'http://localhost:8000/site.html', 'http://localhost:8000/example.html'}}}
+def test_site_map1():
+    input_ = "http://localhost:8000"
+    assert site_map(input_) == {
+        "http://localhost:8000": {
+            "title": "Index",
+            "links": {
+                "http://localhost:8000/site.html",
+                "http://localhost:8000/example.html",
+            },
+        },
+        "http://localhost:8000/site.html": {
+            "title": "The Site",
+            "links": {"http://localhost:8000/site/subsite.html"},
+        },
+        "http://localhost:8000/example.html": {
+            "title": "No links here",
+            "links": set(),
+        },
+        "http://localhost:8000/site/subsite.html": {
+            "title": "Looping",
+            "links": {
+                "http://localhost:8000/site/other_site.html",
+                "http://localhost:8000/",
+            },
+        },
+        "http://localhost:8000/site/other_site.html": {
+            "title": "Looped",
+            "links": {"http://localhost:8000/site/subsite.html"},
+        },
+        "http://localhost:8000/": {
+            "title": "Index",
+            "links": {
+                "http://localhost:8000/site.html",
+                "http://localhost:8000/example.html",
+            },
+        },
+    }
